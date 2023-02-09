c2dc42a88fbfd3d88aff7e1db8763cfec123bdd8
fraserw1 <fraser.wilson.13@gmail.com>
2023-02-09 11:33:09 +0000

    added fade-in / out animations

--- script.js
+++ script.js
@@ -12,25 +12,24 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
-
 // RSVP
 
 const pleaseOpen = document.getElementById("menuButton");
 const close = document.getElementById("menuClose");
 
-pleaseOpen.addEventListener ("click", openMenu);
-close.addEventListener ("click", closeMenu);
+pleaseOpen.addEventListener("click", openMenu);
+close.addEventListener("click", closeMenu);
 
-function openMenu () {
+function openMenu() {
   setTimeout(function () {
-    document.querySelector (".menu").classList.add("menu-open");
-    document.querySelector (".menu2").classList.add("hide");  
+    document.querySelector(".menu").classList.add("menu-open");
+    document.querySelector(".menu2").classList.add("reveal");
   }, 300);
-};
+}
 
-function closeMenu () {
+function closeMenu() {
   setTimeout(function () {
-    document.querySelector (".menu").classList.remove("menu-open");
-    document.querySelector (".menu2").classList.remove("hide");
+    document.querySelector(".menu").classList.remove("menu-open");
+    document.querySelector(".menu2").classList.remove("reveal");
   }, 300);
-};
+}
